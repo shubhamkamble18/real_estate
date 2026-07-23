46a820dedd9e2a1611748aa3c827eb3a88cccc78
shubhamkamble18 <shubhu86520@gmail.com>
2026-07-23 15:19:03 +0530

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+function loggin() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let error = document.getElementById("error");
+  if (email === "admin@123" && password === "123456") {
+    localStorage.setItem("isloggedin", "true");
+    localStorage.setItem("user", "email");
+    window.location.href = "index.html";
+  } else {
+    error.innerText= "invaild email and password";
+  }
+}
+function toggle(){
+    let passinput = document.getElementById("password");
+    if(passinput.type==="password"){
+        passinput.type="text";
+    }
+    else{
+        passinput.type="password"
+    }
+
+}
