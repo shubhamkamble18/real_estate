af43dcd40b93ec760d7dc6f6066aabe7ec6371f1
shubhamkamble18 <shubhu86520@gmail.com>
2026-07-25 11:20:47 +0530

    final touch of fronted

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ function toggle(){
     }
 
 }
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+  if (navLinks.classList.contains("active")){
+    hamburger.innerText = " X ";
+  } 
+  else{
+     hamburger.innerText = " ☰ ";
+  }
+});
